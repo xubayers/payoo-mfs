ff35c9a095af58aa542d5ae8ccc289ac8aa303dd
jubayer <jubayerahmed46@gmail.com>
2024-09-20 11:13:21 +0600

    change home file loacation

--- js/login.js
+++ js/login.js
@@ -18,7 +18,7 @@ loginBtn.addEventListener("click", function (event) {
   }
 
   if (pinNumber.value === "1234") {
-    window.location.href = "http://127.0.0.1:5500/home/home.html";
+    window.location.assign("http://127.0.0.1:5500/home.html");
   } else {
     alert("wrong pin");
   }
